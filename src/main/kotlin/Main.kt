ba76bc0cf58b0e3b1e5360603b02323959fc4fbe
kdm1jkm <kdm1jkmm@gmail.com>
2023-04-02 17:49:16 +0900

    getSearchURL 함수 완성

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,12 @@
+import java.net.URL
+import java.net.URLEncoder
+
 fun main(args: Array<String>) {
     println("Hello World!")
 
-
+    val searchString = "청춘 블"
+    val searchURL = getSearchUrl(searchString)
 }
+
+private fun getSearchUrl(searchString: String): URL =
+    URL("https://comic.naver.com/search?keyword=${URLEncoder.encode(searchString, "UTF-8")}")
